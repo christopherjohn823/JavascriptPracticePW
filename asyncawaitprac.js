5543bbd413387c7f6ec18ff00d7b25645a3715d7
Christopher John <christopher.john823@gmail.com>
2025-07-05 19:56:10 -0400

    commit more changes

--- asyncawaitprac.js
+++ asyncawaitprac.js
@@ -11,3 +11,19 @@ async function showName() {
 }
 
 showName();
+
+// async function completeOrder(){
+//     let coffee = await orderCoffee();
+//     console.log(coffee);
+
+//     let sandwich = await orderSandwich();
+//     console.log(sandwich);
+
+//     let donut = await orderDonut();
+//     console.log(donut);
+
+//     let bill = await getBill();
+//     console.log(bill);
+// }
+
+// completeOrder();
--- removeasyncawaitprac.js
+++ removeasyncawaitprac.js
@@ -1,6 +1,13 @@
+//in this example there is no async/await, so that means it becomes a synchronous function
+//which means it will not wait for 5000 ms. Which means it won't return the 'Sidharth', 
+// which means that the 'name' under 'showName()' will not get the value 'Sidharth'
+// since the 'fetchName()' is synchromous and it will only return the status of the Promise 
+// which is { <pending> } because it is still running. That's why we need async await or .then. 
+// This is what happens when you use Promise without async await or .then.
+
 function fetchName() {
     return new Promise((resolve) => {
-        setTimeout(() => resolve("Sidharth"), 1000);
+        setTimeout(() => resolve("Sidharth"), 5000);
     });
 }
 
